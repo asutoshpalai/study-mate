422c7f897df697d9da1a29b8c187dee36fe3d82c
Asutosh Palai <asupalai@gmail.com>
2015-05-25 15:27:08 +0530

    Changed from erb to slim and added public folder, style.css and an image

--- main.rb
+++ main.rb
@@ -1,39 +1,16 @@
 require 'sinatra'
+require 'slim'
 
 get '/' do
   title = "StudyMate::Home"
-  erb :home, :locals => {:title => title}
+  slim :home, :locals => {:title => title}
 end
 
 get '/about' do
-  erb :about, :locals => {:title => "StudyMata :: About"}
+  slim :about, :locals => {:title => "StudyMata :: About"}
 end
 
-__END__
-@@layout
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title><%if defined? title %><%=title%><%else%>StudyMate<%end%></title>
-</head>
-<body>
-  <header>
-    <h1>Songs By Sinatra</h1>
-    <nav>
-      <ul>
-        <li><a href="/" title="Home">Home</a></li>
-        <li><a href="/about" title="About">About</a></li>
-      </ul>
-    </nav>
-  </header>
-  <section>
-    <%= yield %>
-  </section>
-</body>
-</html>
-@@home
-<p>Welcome to StudyMate, solution to your reference woo</p>
+not_found do
+  slim :not_found
+end
 
-@@about
-<p>This site was created by me to learn sinatra</p>
