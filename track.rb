224c0413a6157c76d5158a015c9cf65340a7142c
Asutosh Palai <asupalai@gmail.com>
2015-06-23 20:52:49 +0530

    Added helpers

--- track.rb
+++ track.rb
@@ -1,7 +1,25 @@
 require './db/track'
 
+module TrackHelpers
+
+  def find_tracks
+    @tracks = Track.all
+  end
+
+  def find_track
+    Track.get(params[:id])
+  end
+
+  def create_track
+    @track = Track.create(params[:track])
+  end
+  
+end
+
+helpers TrackHelpers
+
 get '/tracks' do
-  @tracks = Track.all
+  find_tracks
   slim :tracks
 end
 
@@ -12,32 +30,30 @@ end
 
 post '/tracks/new' do
   halt(401,'Not Authorized') unless session[:admin]
-  track = Track.create(params[:track])
-  redirect to("/track/#{track.id}")
+  create_track
+  redirect to("/track/#{@track.id}")
 end
 
 get '/track/:id' do 
-  @track = Track.get(params[:id])
-  puts @track.inspect
+  @track = find_track
   slim :track_page
 end
 
 delete '/track/:id' do
   halt(401,'Not Authorized') unless session[:admin]
-  @track = Track.get(params[:id])
-  @track.destroy
+  find_track.destroy
   redirect to("/tracks")
 end
 
 get '/track/:id/edit' do
   halt(401,'Not Authorized') unless session[:admin]
-  @track = Track.get(params[:id])
+  @track = find_track
   slim :edit_track
 end
 
 put'/track/:id' do
   halt(401,'Not Authorized') unless session[:admin]
-  track = Track.get(params[:id])
+  track = find_track
   track.update(params[:track])
   redirect to("/track/#{track.id}")
 end
