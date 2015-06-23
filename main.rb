49b95b9871f8b91befcfdca75ab46ba7aff3824f
Asutosh Palai <asupalai@gmail.com>
2015-06-23 16:58:03 +0530

    Added admin login

--- main.rb
+++ main.rb
@@ -3,6 +3,13 @@ require "sinatra/reloader" if development?
 require 'slim'
 require './track'
 
+
+configure do
+  enable :sessions
+  set :username, 'admin'
+  set :password, 'sinatra'
+end
+
 get('/styles.css') { scss :styles }
 
 get '/' do
@@ -16,4 +23,22 @@ end
 
 not_found do
   slim :not_found
+end
+
+get '/login' do
+  slim :login
+end
+
+post '/login' do
+  if params[:username] = settings.username && params[:password] === settings.password
+    session[:admin] = true
+    redirect to('/tracks')
+  else
+    slim :login
+  end
+end
+
+get '/logout' do
+  session.clear
+  redirect to('/')
 end
--- track.rb
+++ track.rb
@@ -6,10 +6,12 @@ get '/tracks' do
 end
 
 get '/tracks/new' do
+  halt(401,'Not Authorized') unless session[:admin]
   slim :new_track
 end
 
 post '/tracks/new' do
+  halt(401,'Not Authorized') unless session[:admin]
   track = Track.create(params[:track])
   redirect to("/track/#{track.id}")
 end
@@ -21,17 +23,20 @@ get '/track/:id' do
 end
 
 delete '/track/:id' do
+  halt(401,'Not Authorized') unless session[:admin]
   @track = Track.get(params[:id])
   @track.destroy
   redirect to("/tracks")
 end
 
 get '/track/:id/edit' do
+  halt(401,'Not Authorized') unless session[:admin]
   @track = Track.get(params[:id])
   slim :edit_track
 end
 
 put'/track/:id' do
+  halt(401,'Not Authorized') unless session[:admin]
   track = Track.get(params[:id])
   track.update(params[:track])
   redirect to("/track/#{track.id}")
